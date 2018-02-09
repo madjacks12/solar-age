b607f2c3b7a54d7d3af787a3cac8cbf9f529e75e
MJ <mdjacksonvideo@gmail.com>
2018-02-09 14:16:26 -0800

    correctly gets user date and splits into year, month and days

--- js/solar-interface.js
+++ js/solar-interface.js
@@ -6,6 +6,12 @@ $(document).ready(function(){
     event.preventDefault();
     let userAge = $('#birthDate').val();
     console.log(userAge);
+    let userYear = (userAge.substring(0,4));
+    console.log(userYear);
+    let userMonth = (userAge.substring(5,7));
+    console.log(userMonth);
+    let userDay = (userAge.substring(8,10));
+    console.log(userDay);
     let newPerson = new Person(userYear, userMonth, userDay);
 
   })
