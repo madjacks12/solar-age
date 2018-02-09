cf40a47a59a9024ba05c75645cc08912d77387b3
MJ <mdjacksonvideo@gmail.com>
2018-02-09 15:18:44 -0800

    correctly parse current time and users birthdate into seperate month, day and year variables.

--- js/solar-interface.js
+++ js/solar-interface.js
@@ -1,18 +1,18 @@
 import { Person } from './../js/solar.js';
 
 $(document).ready(function(){
-  $('#time').text(moment());
+let time = moment(new Date()).format("DD/MM/YYYY")
+let currentDay = parseInt((time.substring(0,2)));
+let currentMonth = parseInt((time.substring(3,5)));
+let currentYear = parseInt((time.substring(6,10)));
   $('#age-form').submit(function(event){
     event.preventDefault();
     let userAge = $('#birthDate').val();
     console.log(userAge);
-    let userYear = (userAge.substring(0,4));
-    console.log(userYear);
-    let userMonth = (userAge.substring(5,7));
-    console.log(userMonth);
-    let userDay = (userAge.substring(8,10));
-    console.log(userDay);
-    let newPerson = new Person(userYear, userMonth, userDay);
+    let userYear = parseInt((userAge.substring(0,4)));
+    let userMonth = parseInt((userAge.substring(5,7)));
+    let userDay = parseInt((userAge.substring(8,10)));
+    let newPerson = new Person(userYear, userMonth, userDay, currentYear, currentMonth, currentDay);
 
   })
 })
--- js/solar.js
+++ js/solar.js
@@ -1,14 +1,17 @@
 export class Person {
-  constructor (userYear, userMonth, userDay) {
+  constructor (userYear, userMonth, userDay, currentYear, currentMonth, currentDay) {
     this.userDay = userDay;
     this.userMonth = userMonth;
     this.userYear = userYear;
+    this.currentYear = currentYear;
+    this.currentMonth = currentMonth;
+    this.currentDay = currentDay;
   }
 
 
-  // convertToSeconds() {
-  //   const day = 86400
-  //   const month =2592000
-  //   const year = 31536000
-  // }
+  convertToSeconds() {
+    const day = 86400
+    const month =2592000
+    const year = 31536000
+  }
 };
